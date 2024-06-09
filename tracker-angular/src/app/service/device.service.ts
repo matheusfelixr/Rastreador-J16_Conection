7e821ceadb4233d6c5ddef13c2e452f5d7da7dea
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2024-06-09 00:23:17 -0300

    main

--- tracker-angular/src/app/service/device.service.ts
+++ tracker-angular/src/app/service/device.service.ts
@@ -18,37 +18,37 @@ export class DeviceService {
   const fakeDevices: Device[] = [
     { 
       id: 1, 
-      nickname: 'Device 1', 
-      carName: 'Car 1', 
-      licensePlate: 'ABC123', 
+      nickname: 'Classic j16', 
+      carName: 'GM/Classic 2012', 
+      licensePlate: 'OLV-9947', 
       latitude: 10.1234, 
       longitude: -20.5678, 
       imageUrl: 'https://italo.com.br/wp-content/uploads/2024/01/garoto-de-tiro-medio-trapaceando-no-teste-da-escola.jpg', 
       imei: '123456789012345', // IMEI fictício
-      chip: '123456789', // Número do chip fictício
-      phone: '+1234567890' // Número de telefone fictício
-    },   { 
-      id: 1, 
-      nickname: 'Device 1', 
-      carName: 'Car 1', 
-      licensePlate: 'ABC123', 
+      chip: 'TIM', // Número do chip fictício
+      phone: '+34 99186-7176' // Número de telefone fictício
+    },{ 
+      id: 2, 
+      nickname: 'Corsa j16', 
+      carName: 'GM/Corsa 2012', 
+      licensePlate: 'AJY-7586', 
       latitude: 10.1234, 
       longitude: -20.5678, 
       imageUrl: 'https://italo.com.br/wp-content/uploads/2024/01/garoto-de-tiro-medio-trapaceando-no-teste-da-escola.jpg', 
       imei: '123456789012345', // IMEI fictício
-      chip: '123456789', // Número do chip fictício
-      phone: '+1234567890' // Número de telefone fictício
-    },   { 
-      id: 1, 
-      nickname: 'Device 1', 
-      carName: 'Car 1', 
-      licensePlate: 'ABC123', 
+      chip: 'OI', // Número do chip fictício
+      phone: '+34 99123-9845' // Número de telefone fictício
+    },{ 
+      id: 3, 
+      nickname: 'S10 j16', 
+      carName: 'GM/S10 2012', 
+      licensePlate: 'WLV-9577', 
       latitude: 10.1234, 
       longitude: -20.5678, 
       imageUrl: 'https://italo.com.br/wp-content/uploads/2024/01/garoto-de-tiro-medio-trapaceando-no-teste-da-escola.jpg', 
       imei: '123456789012345', // IMEI fictício
-      chip: '123456789', // Número do chip fictício
-      phone: '+1234567890' // Número de telefone fictício
+      chip: 'CLARO', // Número do chip fictício
+      phone: '+34 99656-7234' // Número de telefone fictício
     },
     ];
 
