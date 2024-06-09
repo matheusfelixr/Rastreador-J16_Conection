c8be17b8c95e81f86d5ee058884b4bf62110923d
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2024-06-09 01:45:36 -0300

    main

--- tracker/src/main/java/com/mfr/tracker/model/Device.java
+++ tracker/src/main/java/com/mfr/tracker/model/Device.java
@@ -1,10 +1,18 @@
 package com.mfr.tracker.model;
 
 import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Data
+@Entity
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nickname;
     private String carName;
     private String licensePlate;
